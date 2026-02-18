5e6356362e0593dac1d49bf6661d1c6f2b65fae7
IG <igonzalez1466@gmail.com>
2026-02-18 18:00:13 +0100

    Move BookingForm to _components

--- app/listing/[id]/_components/BookingForm.tsx
+++ app/listing/[id]/_components/BookingForm.tsx
@@ -2,7 +2,7 @@
 
 import { useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
-import { createBookingAction } from "./actions";
+import { createBookingAction } from "../actions";
 
 export default function BookingForm({
   listingId,
